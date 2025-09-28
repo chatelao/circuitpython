60327c8569a7e11b50443816d166287a7880cbcb
chatelao <olivier.chatelain@gmail.com>
2025-09-28 21:55:40 +0200

    Update __init__.c - neopixel with 5V pull-up resistory
    Next try wit AI generated code (what could possibly go wrong ....

--- ports/raspberrypi/common-hal/neopixel_write/__init__.c
+++ ports/raspberrypi/common-hal/neopixel_write/__init__.c
@@ -21,13 +21,13 @@ uint64_t next_start_raw_ticks = 0;
 // instructions run per bit. We start with the low period because it can be
 // longer while waiting for more data.
 const uint16_t neopixel_program[] = {
-    0xf081, //  0: set    pindirs, 1      side 0
-    0x7521, //  1: out    x, 1            side 0 [5]
-    0xf080, //  2: set    pindirs, 0      side 0
-    0x1a26, //  3: jmp    !x, 6           side 1 [2]
-    0xfb80, //  4: set    pindirs, 0      side 1 [3]
-    0x1800, //  5: jmp    0               side 1 
-    0xb442, //  6: nop                    side 0 [4]
+    0x7021, //  0: out    x, 1                   [16]
+    0x0025, //  1: jmp    !x, 5
+    0xe580, //  2: set    pindirs, 0             [5]
+    0xe381, //  3: set    pindirs, 1             [3]
+    0x0000, //  4: jmp    0
+    0xe280, //  5: set    pindirs, 0             [2]
+    0xe681, //  6: set    pindirs, 1             [6]
 };
 
 void common_hal_neopixel_write(const digitalio_digitalinout_obj_t *digitalinout, uint8_t *pixels, uint32_t num_bytes) {
